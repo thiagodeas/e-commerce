7b45bdb160944ee9f13182932f6f88892b5cc70b
thiagodeas <sousaalvesth@gmail.com>
2025-02-21 11:23:52 -0300

    alterando layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
-      <body>
+      <body className="bg-[#f3f4f6]">
         <Header />
         {children}
       </body>
