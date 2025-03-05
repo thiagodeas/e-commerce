a43b34b9c574e771b8ce146080038239cc97a58d
thiagodeas <sousaalvesth@gmail.com>
2025-03-05 19:40:55 -0300

    feat: adicionando carrossel na homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,48 @@
-import Head from "next/head";
+"use client"
+
+import { Card, CardContent } from "@/components/ui/card"
+
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
+import React from "react";
+import Autoplay from 'embla-carousel-autoplay';
 
 export default function HomePage() {
-  return (
-    <Head>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
+    const plugin = React.useRef(
+      Autoplay({ delay: 6000, stopOnInteraction: true })
+    );
     
+  return (
+    <div className="w-full min-h-full flex items-center justify-center text-center">
+
+         <Carousel
+      plugins={[plugin.current]}
+      className="w-full flex items-center justify-center"
+      onMouseEnter={plugin.current.stop}
+      onMouseLeave={plugin.current.reset}
+    >
+      <CarouselContent>
+        {Array.from({ length: 3 }).map((_, index) => (
+          <CarouselItem key={index} className="w-full">
+            <div className="w-full">
+              <Card>
+                <CardContent>
+                  <img src="https://placehold.co/1350x400" alt="Promoções" className="w-full h-auto" />
+                </CardContent>
+              </Card>
+            </div>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious className="absolute left-12 top-1/2 transform -translate-y-1/2"/>
+      <CarouselNext className="absolute right-12 top-1/2 transform -translate-y-1/2"/>
+    </Carousel>
+
+    </div>
   );
 }
