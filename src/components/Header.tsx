9ddc65b72b21e68882c9e998130330fbb6f1ae59
thiagodeas <sousaalvesth@gmail.com>
2025-03-05 19:39:58 -0300

    feat: adicionando mais ferramentas ao Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -21,7 +21,7 @@ export default function Header () {
 
             <div className="flex items-center justify-around w-full pt-12">
                 <div className="flex items-center gap-2">
-                    <h1 className="text-4xl font-bold mb-4 text-[#4F46E5]">ClickFash</h1>
+                    <h1 className="text-4xl font-bold mb-4 text-[#4F46E5]">ClickShop</h1>
                     <GiClick className="w-[40px] h-[40px] text-[#4F46E5]"/>
                 </div>
 
@@ -33,16 +33,18 @@ export default function Header () {
                         className="px-4 border-[#1E293B] border rounded-full focus:outline-none focus:border-none focus:ring-2 focus:ring-[#6366F1]"
                         />
                     </div> 
-                    <Link href='#'> <FaShoppingCart className="w-[25px] h-[25px]"/> </Link>
+                    <Link href='#'> <FaShoppingCart className="w-[25px] h-[25px] text-[#1E293B]"/> </Link>
                 </nav>
                 
             </div>
 
-            <div className="flex items-center justify-center gap-8 w-full pt-8 text-[20px]">
+            <div className="flex items-center justify-center gap-8 w-full pt-8 text-[20px] text-[#1E293B]">
                 <Link href='#'> Moda e Acessórios </Link>
                 <Link href='#'> Beleza e Cuidados Pessoais </Link>
                 <Link href='#'> Casa e Cozinha </Link>
                 <Link href='#'> Esportes e Lazer </Link>
+                <Link href='#'> Eletrônicos </Link>
+                <Link href='#'> Livros e Papelaria </Link>
             </div>
         </header>
     );
