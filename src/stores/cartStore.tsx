0a5d0b9b7c5681ecc07e6df54c8773daa7fbdf8b
thiagodeas <sousaalvesth@gmail.com>
2025-03-22 09:58:14 -0300

    feat: modificando metodos do store de cart

--- src/stores/cartStore.tsx
+++ src/stores/cartStore.tsx
@@ -6,13 +6,16 @@ interface Product {
     name: string;
     price: number;
     imageUrl: string;
+    quantity: number;
 }
 
 interface CartStore {
     cart: Product[];
     addToCart: (product: Product) => void;
     removeFromCart: (id: string) => void;
+    updateQuantity: (id: string, quantity: number) => void;
     getCartCount: () => number;
+    getSubtotal: () => number;
 }
 
 export const useCartStore = create<CartStore>()(
@@ -20,13 +23,44 @@ export const useCartStore = create<CartStore>()(
         (set, get) => ({
             cart: [],
 
-            addToCart: (product) =>
-                set((state) => ({ cart: [...state.cart, product] })),
+            addToCart: (product) => {
+                const cart = get().cart;
+                const existingProduct = cart.find((item) => item._id === product._id);
 
-            removeFromCart: (id) =>
-                set((state) => ({ cart: state.cart.filter((item) => item._id !== id) })),
+                if (existingProduct) {
+                    set({
+                        cart: cart.map((item) =>
+                            item._id === product._id
+                                ? { ...item, quantity: item.quantity + 1 }
+                                : item
+                        ),
+                    });
+                } else {
+                    set({ cart: [...cart, { ...product, quantity: 1 }] });
+                }
+            },
 
-            getCartCount: () => get().cart.length,
+            removeFromCart: (id) => {
+                set((state) => ({
+                    cart: state.cart.filter((item) => item._id !== id),
+                }));
+            },
+
+            updateQuantity: (id, quantity) => {
+                set((state) => ({
+                    cart: state.cart.map((item) =>
+                        item._id === id
+                            ? { ...item, quantity: Math.max(quantity, 1) } 
+                            : item
+                    ),
+                }));
+            },
+
+            getCartCount: () => get().cart.reduce((total, item) => total + item.quantity, 0),
+
+            getSubtotal: () => {
+                return get().cart.reduce((total, item) => total + item.price * item.quantity, 0);
+            },
         }),
         {
             name: "cart-storage",
